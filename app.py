1144c470d0ff7fef923b48317de5a97194ffdec0
Rok <rok.ankaran@gmail.com>
2024-06-21 14:39:42 +0200

    run crawler synchronus

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from database_create import create_websites_db_if_not_exists
 from crawler import Crawler
-import asyncio
-from database import WebsitesDatabase
 
 #Create keyspace and tables for websites database if they dont exist
 create_websites_db_if_not_exists()
 
 #Run the crawler
-db = WebsitesDatabase()
-
-crawler = Crawler(db.get_urls_to_crawl())
-asyncio.run(crawler.crawl())
+crawler = Crawler()
+crawler.crawl()
