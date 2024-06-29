120b83ad444fe797cf213969a1e0188da14d7931
Rok <rok.ankaran@gmail.com>
2024-06-29 14:24:27 +0200

    corrected indentation

--- crawler.py
+++ crawler.py
@@ -7,8 +7,8 @@ from urllib.parse import urlparse, urljoin
 from datetime import datetime, timezone
 
 def get_utc_timestamp():
-    current_time_utc = datetime.now(timezone.utc)
-    return current_time_utc.timestamp()
+  current_time_utc = datetime.now(timezone.utc)
+  return current_time_utc.timestamp()
 
 class Crawler():
   def __init__(self) -> None:
@@ -19,22 +19,22 @@ class Crawler():
     while True:
       urls_to_crawl = self.database.get_urls_to_crawl() #Get the urls to crawl from db
       if len(urls_to_crawl) == 0:
-         return 1 #Quit if there aren't any urls to crawl
+        return 1 #Quit if there aren't any urls to crawl
 
       url = urls_to_crawl[0]
 
       #Don't crawl this url if it was crawled in the last hour or it is a pdf
       if (url in self.recenty_crawled and get_utc_timestamp() - self.recenty_crawled[url] < 3600) or (url[-4:] == ".pdf"):
-         self.database.remove_url_to_crawl(url)
-         continue
+        self.database.remove_url_to_crawl(url)
+        continue
 
       print(f"Crawling {url}")
 
       try:
         page_info = self.extract_page_info(url)
       except Exception as e:
-         self.database.remove_url_to_crawl(url)
-         continue
+        self.database.remove_url_to_crawl(url)
+        continue
 
       self.database.insert_website(url, page_info["title"], page_info["keywords"])
       self.database.remove_url_to_crawl(url)
@@ -55,26 +55,26 @@ class Crawler():
     #Get all links in page for further crawling
     discovered_links_on_this_site = set()
     for link in soup.find_all("a"):
-        #If the link is valid add it to database to crawl
-        link_url = link.get("href")
+      #If the link is valid add it to database to crawl
+      link_url = link.get("href")
+      parsed_url = urlparse(link_url)
+      if parsed_url.scheme and parsed_url.netloc and link_url != url and link_url not in discovered_links_on_this_site:
+        self.add_url_to_crawl(link_url)
+        discovered_links_on_this_site.add(link_url)
+      else:
+        #Link isn't valid so try merging the link with the base url, example: merge "https://www.google.com/"(base url) and "/search"
+        #Then you get "https://www.google.com/search" and check if this merged url is valid
+        link_url = urljoin(url, link_url)
         parsed_url = urlparse(link_url)
         if parsed_url.scheme and parsed_url.netloc and link_url != url and link_url not in discovered_links_on_this_site:
           self.add_url_to_crawl(link_url)
           discovered_links_on_this_site.add(link_url)
-        else:
-          #Link isn't valid so try merging the link with the base url, example: merge "https://www.google.com/"(base url) and "/search"
-          #Then you get "https://www.google.com/search" and check if this merged url is valid
-          link_url = urljoin(url, link_url)
-          parsed_url = urlparse(link_url)
-          if parsed_url.scheme and parsed_url.netloc and link_url != url and link_url not in discovered_links_on_this_site:
-            self.add_url_to_crawl(link_url)
-            discovered_links_on_this_site.add(link_url)
 
     return {
-       "url": url,
-       "title": title,
-       "content": user_visible_content,
-       "keywords": keywords
+      "url": url,
+      "title": title,
+      "content": user_visible_content,
+      "keywords": keywords
     }
 
   def get_user_visible_content(self, soup:BeautifulSoup) -> str:
@@ -86,18 +86,18 @@ class Crawler():
     contains_alpha = False
     for char in user_visible_content:
       if char.isalpha():
-          contains_alpha = True
-          exit
+        contains_alpha = True
+        exit
 
     #Translate text to english if contains ANY alpha chars
     if contains_alpha:
       language = translate.detect_lang(user_visible_content) #Get text language
 
       if language != "en": #Translate if the language isnt english
-          translated_text = translate.translate(user_visible_content, language)
+        translated_text = translate.translate(user_visible_content, language)
 
-          if type(translated_text) == str: #Set the translated text to user_visible_content only if the trnslated text is a string and not an error message
-              user_visible_content = translated_text
+        if type(translated_text) == str: #Set the translated text to user_visible_content only if the trnslated text is a string and not an error message
+          user_visible_content = translated_text
     
     return user_visible_content
   
